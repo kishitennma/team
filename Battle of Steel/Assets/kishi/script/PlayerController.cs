62f98b1897bf6c5bcb3bcc38a360676652f5d8b5
DESKTOP-M30FNO0\GA2B <h1202419012@hiratagakuen.onmicrosoft.com>
2025-04-23 14:20:30 +0900

    三人称視点

--- Battle of Steel/Assets/kishi/script/PlayerController.cs
+++ Battle of Steel/Assets/kishi/script/PlayerController.cs
@@ -1,123 +1,58 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float moveSpeedIn;//�v���C���[�̈ړ����x�����
+    [SerializeField] GameObject player;
 
+    public GameObject Cam;
+    private Animator animator; // キャラクターオブジェクトのAnimator
+    public RuntimeAnimatorController walking;
+    public RuntimeAnimatorController running;
+    public RuntimeAnimatorController standing;
 
-    Rigidbody playerRb;//�v���C���[��Rigidbody
+    public float moveSpeed = 5.0f; // キャラクターの移動速度
 
-    Vector3 moveSpeed;//�v���C���[�̈ړ����x
+    public bool damaged;
 
-    Vector3 currentPos;//�v���C���[�̌��݂̈ʒu
-    Vector3 pastPos;//�v���C���[�̉ߋ��̈ʒu
-
-    Vector3 delta;//�v���C���[�̈ړ���
-
-    Quaternion playerRot;//�v���C���[�̐i�s�����������N�H�[�^�j�I��
-
-    float currentAngularVelocity;//���݂̉�]�e���x
-
-    [SerializeField]
-    float maxAngularVelocity = Mathf.Infinity;//�ő�̉�]�p���x[deg/s]
-
-    [SerializeField]
-    float smoothTime = 0.1f;//�i�s�����ɂ����邨���悻�̎���[s]
-
-    float diffAngle;//���݂̌����Ɛi�s�����̊p�x
-
-    float rotAngle;//���݂̉�]����p�x
-
-    Quaternion nextRot;//�ǂ񂭂炢��]���邩
-    void Start()
+    private void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        damaged = false;
+    }
 
-        pastPos = transform.position;
+    void Screen_movement(float mx)
+    {
+        // X方向に一定量移動していれば横回転
+        //0.0000001fは滑らかさ
+        if (Mathf.Abs(mx) > 0.0000001f)
+        {
+            mx = mx * 5;
+
+            // 回転軸はワールド座標のY軸
+            player.transform.RotateAround(player.transform.position, Vector3.up, mx);
+        }
     }
 
     void Update()
     {
-        //------�v���C���[�̈ړ�------
+        float mx = Input.GetAxis("Mouse X");
+        Screen_movement(mx);
 
-        //�J�����ɑ΂��đO���擾
-        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        //�J�����ɑ΂��ĉE���擾
-        Vector3 cameraRight = Vector3.Scale(Camera.main.transform.right, new Vector3(1, 0, 1)).normalized;
-
-        //moveVelocity��0�ŏ���������
-        moveSpeed = Vector3.zero;
-
-        //�ړ�����
         if (Input.GetKey(KeyCode.W))
         {
-            moveSpeed = moveSpeedIn * cameraForward;
-        }
+            // "W"キーが押されたときの処理をここに記述
+            print("歩くぞお");
+            animator.runtimeAnimatorController = walking;
 
-        if (Input.GetKey(KeyCode.A))
+            //プレイヤーの正面に向かって移動する
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
         {
-            moveSpeed = -moveSpeedIn * cameraRight;
+            animator.runtimeAnimatorController = standing;
         }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveSpeed = -moveSpeedIn * cameraForward;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveSpeed = moveSpeedIn * cameraRight;
-        }
-
-        //Move���\�b�h�ŁA�͉����Ă��炤
-        Move();
-
-        //����������
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
-        {
-            //playerRb.velocity = Vector3.zero;
-            // playerRb.angularVelocity = Vector3.zero;
-        }
-
-        //------�v���C���[�̉�]------
-
-        //���݂̈ʒu
-        currentPos = transform.position;
-
-        //�ړ��ʌv�Z
-        delta = currentPos - pastPos;
-        delta.y = 0;
-
-        //�ߋ��̈ʒu�̍X�V
-        pastPos = currentPos;
-
-        if (delta == Vector3.zero)
-            return;
-
-        playerRot = Quaternion.LookRotation(delta, Vector3.up);
-
-        diffAngle = Vector3.Angle(transform.forward, delta);
-
-        //Vector3.SmoothDamp��Vector3�^�̒l�����X�ɕω�������
-        //Vector3.SmoothDamp (���ݒn, �ړI�n, ref ���݂̑��x, �J�ڎ���, �ō����x);
-        rotAngle = Mathf.SmoothDampAngle(0, diffAngle, ref currentAngularVelocity, smoothTime, maxAngularVelocity);
-
-        nextRot = Quaternion.RotateTowards(transform.rotation, playerRot, rotAngle);
-
-        transform.rotation = nextRot;
-
-    }
-
-    /// <summary>
-    /// �ړ������ɗ͂�������
-    /// </summary>
-    private void Move()
-    {
-        //playerRb.AddForce(moveSpeed, ForceMode.Force);
-
-        playerRb.linearVelocity = moveSpeed;
     }
 }
