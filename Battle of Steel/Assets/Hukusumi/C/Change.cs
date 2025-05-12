7d17ff6e027d7abc3ea8c54dfdfd1c7082b79475
DESKTOP-U0TU0SG\GA2B <h1202415011@hiratagakuen.onmicrosoft.com>
2025-05-12 15:46:39 +0900

    武器変更時の回転アニメーション

---
+++ Battle of Steel/Assets/Hukusumi/C/Change.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Change : MonoBehaviour
+{
+    public float CKS = 1;
+    public bool C=false;
+    RectTransform RectTransform_get;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+        RectTransform_get = gameObject.GetComponent<RectTransform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (C == false)
+        {
+            RectTransform_get.Rotate(new Vector3(0, 0, CKS));
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            float rotationZ = rectTransform.eulerAngles.z;
+
+            if (rotationZ > 180f) // ó·Ç¶ÇŒ45ìxà»è„Ç»ÇÁèåèê¨óß
+            {
+                RectTransform_get.rotation = Quaternion.Euler(0, 0, 0);
+                C = true;
+
+            }
+
+        }
+    }
+}
