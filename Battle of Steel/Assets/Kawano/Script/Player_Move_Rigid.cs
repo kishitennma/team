55c4e39a5c513ce6a8a2fd833dcdaa570e53afa3
KawanoTakumi <h1202416001@hiratagakuen.onmicrsoft.com>
2025-05-30 13:44:19 +0900

    プレイヤーが壁を貫通しなくなりました

--- Battle of Steel/Assets/Kawano/Script/Player_Move_Rigid.cs
+++ Battle of Steel/Assets/Kawano/Script/Player_Move_Rigid.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Move_Rigid : MonoBehaviour
@@ -11,10 +12,11 @@ public class Player_Move_Rigid : MonoBehaviour
     private Rigidbody rb;//RigidBody
     private float move_x, move_y;//線形補完用
     private float target_x, target_y;//線形保管用
+    private bool wall_flag;
 
     private Vector3 input_direction;//入力方向
 
-    //UnityのRigidBodyを使った場合のプレイヤーの移動方法
+    //UnityのRigidBodyを使った場合のプレイヤーの移動方法 :: 実験段階
 
 
     void Start()
@@ -27,32 +29,9 @@ public class Player_Move_Rigid : MonoBehaviour
 
     void Update()
     {
+        move_x = 0;move_y = 0;
         float mx = Input.GetAxis("Mouse X");
-        //入力を受け取る
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
 
-        Vector3 move_dir = (transform.right * h + transform.forward * v).normalized;
-        float move_distance = move_speed * Time.fixedDeltaTime;
-
-        //移動方向にRigidBody持ちのオブジェクトがなかったら
-        if(!Physics.Raycast(transform.position,move_dir,move_distance + 0.1f))
-        {
-            //移動速度を設定
-            if (!Input.GetKey(KeyCode.LeftShift))
-            {
-                //通常時
-                player_animator.SetFloat("IsDashing", 0.0f);
-                input_direction = transform.right * h + transform.forward * v;
-            }
-            else
-            {
-                //ダッシュ時
-                player_animator.SetFloat("IsDashing", 1.0f);//Animatorをダッシュに切り替え
-                input_direction = transform.right * h + transform.forward * dash_speed;//移動ベクトルを設定
-            }
-
-        }
         Screen_movement(mx);
 
         //押されているボタンによって、アニメーションを制御
@@ -84,16 +63,55 @@ public class Player_Move_Rigid : MonoBehaviour
         player_animator.SetFloat("Vertical", move_y);
 
     }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("何かに当たった");
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            
+            //最終手段
+            //wall_flag = true;
+            //input_direction = Vector3.zero;
+        }
 
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+            wall_flag = false;
 
+    }
     private void FixedUpdate()
     {
-        //入力がある時のみ、移動を許可
-        if(input_direction != Vector3.zero)
+        //入力を受け取る
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move_dir = (transform.right * h + transform.forward * v).normalized;
+        Vector3 origin = transform.position + Vector3.up * 0.5f;
+        float move_distance = move_speed * Time.fixedDeltaTime;
+        float radius = 0.4f;
+        //移動方向にRigidBody持ちのオブジェクトがなかったら
+        if (!Physics.SphereCast(origin,radius, move_dir,out RaycastHit hit,  move_distance + 0.1f) && wall_flag != true)
         {
+            move_dir = Vector3.ProjectOnPlane(input_direction, hit.normal).normalized;
+            //移動速度を設定
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                //通常時
+                player_animator.SetFloat("IsDashing", 0.0f);
+                input_direction = transform.right * h + transform.forward * v;
+            }
+            else
+            {
+                //ダッシュ時
+                player_animator.SetFloat("IsDashing", 1.0f);//Animatorをダッシュに切り替え
+                input_direction = (transform.right * h + transform.forward * v) * dash_speed;//移動ベクトルを設定
+            }
             //移動方向を設定
             Vector3 move_offset = input_direction * move_speed * Time.deltaTime;
             rb.MovePosition(rb.position + move_offset);//RigidBody自体の位置を移動
+
         }
     }
 
