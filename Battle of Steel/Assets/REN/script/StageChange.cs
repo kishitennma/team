d7a965c572560526bd92c2e6756e88719b183351
DESKTOP-U0TU0SG\GA2B <reny58817@gmail.com>
2025-06-25 15:14:34 +0900

    ステージスクリプト(仮)

--- Battle of Steel/Assets/REN/script/StageChange.cs
+++ Battle of Steel/Assets/REN/script/StageChange.cs
@@ -4,6 +4,7 @@ public class StageChange : MonoBehaviour
 {
     //ボス登場フラグ
     private bool boss_spawned = false;
+    public GameObject Building;
 
     void Update()
     {
@@ -14,12 +15,13 @@ public class StageChange : MonoBehaviour
             GameObject[] Buildings = GameObject.FindGameObjectsWithTag("Building");
 
             //GameObject型の変数moveに、cubesの中身を順番に取り出す
-            //foreachは配列の要素の数だけループします
+            //foreachは配列の要素の数だけループ
             foreach (GameObject move in Buildings)
             {
                 //それらを下に直線移動
-                //this.transform.positionへ毎フレーム一定の数値を足す
-                this.transform.position += new Vector3(0, -1.0f, 0);
+                //Building.transform.positionへ毎フレーム一定の数値を足す
+                Building.transform.position += new Vector3(0, -1.0f, 0);
+
             }
         }
     }
