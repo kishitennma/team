1040ee0071be3252c4134c5501a7dfd8711fa39c
DESKTOP-64O22OL\GA2B <h1202415011@hiratagakuen.onmicrosoft.com>
2025-07-02 13:31:17 +0900

    まうすりせっと

---
+++ Battle of Steel/Assets/Hukusumi/Script/MouseReset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseReset : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //É}ÉEÉXèâä˙âª
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
